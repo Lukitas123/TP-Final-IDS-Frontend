aac86d2ed3a9c17eeeb801048a4ef0c99870b821
Florian Lucchini <florian.lucchini.ar@gmail.com>
2025-11-10 03:48:14 -0300

    Adding function for gallery parsing, from "['url', 'url', 'url']" to ["url", "url", ...]

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
 from flask_mail import Message
+from typing import Dict, List, Any
+import ast
 
 def enviar_email_contacto(mail, datos_formulario, archivo_adjunto):
     try:
@@ -36,3 +38,17 @@ def enviar_email_contacto(mail, datos_formulario, archivo_adjunto):
     except Exception as e:
         print(f"Error al enviar el correo desde functions.py: {e}")
         return False
+
+def parse_gallery(dicts_list: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    proc_list: List[Dict[str, Any]] = []
+    for item in dicts_list:
+        copied_item = item.copy()
+        if "galeria" in copied_item and isinstance(copied_item["galeria"], str):
+            try:
+                copied_item["galeria"] = ast.literal_eval   (copied_item["galeria"])
+            except Exception as e:
+                print(f"Error parsing gallery for item {item.get('id', 'unknown')}: {e}")
+                copied_item["galeria"] = []
+                
+        proc_list.append(copied_item)
+    return proc_list

807d73a4356eaf812be3caa2807c65b972458f43
Lukitas123 <Penefflukas19@gmail.com~>
2025-11-11 20:35:59 -0300

    Implementé la integracion con las variables de entorno y flask-mail

--- app.py
+++ app.py
@@ -3,11 +3,20 @@ from flask_mail import Mail
 from config import Config 
 from functions import enviar_email_contacto, parse_gallery
 from hardcoded_data import DATA
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
-app.config.from_object(Config)
-
+# --- CONFIGURACIÓN DE FLASK-MAIL ---
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))
+app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', 'true').lower() in ['true', '1', 't']
+app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL', 'false').lower() in ['true', '1', 't']
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER', os.getenv('MAIL_USERNAME')) 
 mail = Mail(app)
 
 
